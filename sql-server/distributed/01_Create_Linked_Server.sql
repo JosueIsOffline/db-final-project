6f546908505f3e18b926ce06bdc3e15c56d2d181
IsmarlinM <ismarlinm324@gmail.com>
2025-04-22 14:19:49 -0400

    change in the description of 01_Create_Linked_Server

--- sql-server/distributed/01_Create_Linked_Server.sql
+++ sql-server/distributed/01_Create_Linked_Server.sql
@@ -3,14 +3,33 @@
     Final Project - Advanced Database
     ITLA
 
-	This script creates a linked server named 'LOCAL' that connects
-    the current SQL Server instance to a remote server named 'DESKTOP-2JU6T45', 
-    where the database RetailChainDB is hosted.
+    --Descripción:
+    Este script configura un servidor vinculado denominado LOCAL en SQL Server, 
+    permitiendo que la instancia actual de SQL Server se conecte a sí misma. 
+    Esto facilita la ejecución de consultas distribuidas dentro del entorno de la 
+    base de datos RetailChainDB, habilitando el acceso a datos entre diferentes servidores.
 
-    Purpose:
-    - Enable distributed queries across servers
-    - Facilitate modular architecture for Inventory, 
-      HR, Sales, and other schemas
+    --Propósito:
+    - Crear un Linked Server 'LOCAL' en la instancia actual.
+    - Habilitar consultas distribuidas entre servidores en RetailChainDB.
+
+    --Acciones del script:
+    1.Obtención del nombre del servidor actual: Utiliza la función @@SERVERNAME para 
+    identificar el nombre de la instancia de SQL Server.
+    2.Creación del servidor vinculado: Ejecuta el procedimiento almacenado sp_addlinkedserver 
+    para configurar el servidor vinculado LOCAL utilizando el proveedor SQLNCLI11.
+    3.Confirmación: Imprime un mensaje confirmando la creación exitosa del servidor vinculado.
+
+    --Detalles del Script:
+    - @server: Nombre del Likned Server ('LOCAL').
+    - @srvproduct: Producto del servidor (se deja vacóo ya que es SQL Serve).
+    - @provider: Proveedor OLE DB ('SQLNCLI11') utilizado para la conexión..
+    - @datasrc: El nombre de la instancia actual de SQL Server, obtenido dinámicamente 
+      mediante @@SERVERNAME.
+
+    --Instrucciones:
+    1. Ejecute el script en la base de datos 'master'.
+    2. Verifique el mensaje de confirmación de éxito.
 */
 
 USE master
@@ -19,8 +38,12 @@ GO
 DECLARE @CurrentServerName NVARCHAR(128)
 DECLARE @SQL NVARCHAR(1000)
 
+
+-- Obtener el nombre del servidor actual
 SELECT @CurrentServerName = @@SERVERNAME
 
+
+-- Configurar el servidor vinculado 'LOCAL'
 SET @SQL = N'
 EXEC sp_addlinkedserver 
     @server = ''LOCAL'', 
@@ -28,9 +51,11 @@ EXEC sp_addlinkedserver
     @provider = ''SQLNCLI11'',
     @datasrc = ''' + @CurrentServerName + ''';'
 
+-- Ejecutar la creación del Linked Server
 EXECUTE sp_executesql @SQL
 
 
+-- Confirmar que el Linked Server ha sido creado exitosamente
 PRINT 'Linked Server LOCAL created successfully with datasrc = ' + @CurrentServerName;
 GO
 
